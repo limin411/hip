2968356bc7c6d7f9421aa31b1a3ffe0a80d3be1a
jiaminli <lijiamin411@gmail.com>
2026-06-07 10:46:19 +0800

    feat(i18n): add i18next config and translation files for zh-CN, zh-TW, en

---
+++ src/i18n/index.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { zhCN } from './zh-CN'
+import { zhTW } from './zh-TW'
+import { en } from './en'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      'zh-CN': zhCN,
+      'zh-TW': zhTW,
+      en,
+    },
+    fallbackLng: 'zh-CN',
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
